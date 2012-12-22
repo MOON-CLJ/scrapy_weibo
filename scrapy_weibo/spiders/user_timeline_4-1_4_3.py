ed45716ad1fad2a5357753ca9d2db4bbfd164feb
MOON_CLJ <lijunli2598@gmail.com>
2012-12-22 20:28:58 +0800

    调整user_timeline_4-1_4-3

--- scrapy_weibo/spiders/user_timeline_4-1_4_3.py
+++ scrapy_weibo/spiders/user_timeline_4-1_4_3.py
@@ -1,16 +1,19 @@
-import redis
+# -*- coding: utf-8 -*-
+
 import simplejson as json
 from scrapy.spider import BaseSpider
-from scrapy_weibo.items import WeiboItem, UserItem
+from utils4scrapy.utils import resp2item_v2
+from utils4scrapy.tk_maintain import _default_redis, _default_req_count, _default_tk_alive, \
+    EXPIRED_TOKEN, INVALID_ACCESS_TOKEN
 from scrapy import log
 from scrapy.conf import settings
 from scrapy.exceptions import CloseSpider
 from scrapy.http import Request
 
 
-# weibo apis default extras config
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
+API_KEY = '4131380600'
 UIDS_SET = '{spider}:uids'
 BASE_URL = 'https://api.weibo.com/2/statuses/user_timeline.json?uid={uid}&page={page}&since_id={since_id}&max_id={max_id}&count=100'
 
@@ -18,8 +21,8 @@ BASE_URL = 'https://api.weibo.com/2/statuses/user_timeline.json?uid={uid}&page={
 class UserTimelineApril(BaseSpider):
     name = "user_timeline_april"
     r = None
-    since_id = 3421438975589423
-    max_id = 3438780670993204
+    since_id = 3513504881782892
+    max_id = 3523483302151071
 
     def start_requests(self):
         uids = self.prepare()
@@ -35,79 +38,67 @@ class UserTimelineApril(BaseSpider):
             yield request
 
     def parse_next(self, response):
-        resp = json.loads(response.body)
         page = response.meta['page']
         retry = response.meta['retry']
         uid = response.meta['uid']
 
-        if "error_code" in resp and resp["error_code"] in [21314, 21315, 21316, 21317]:
-            raise CloseSpider('ERROR: TOKEN NOT VALID')
+        resp = json.loads(response.body)
+        if response.status != 200:
+            if resp.get('error_code') in [EXPIRED_TOKEN, INVALID_ACCESS_TOKEN]:
+                token = response.request.headers['Authorization'][7:]
+                self.req_count.delete(token)
+                self.tk_alive.drop_tk(token)
 
-        if 'statuses' in resp and resp['statuses'] == []:
-            retry += 1
-            if retry > 2:
+                retry += 1
+                if retry > 2:
+                    return
+
+                request = Request(BASE_URL.format(uid=uid, page=page,
+                                  since_id=self.since_id, max_id=self.max_id), headers=None,
+                                  callback=self.parse_next, dont_filter=True)
+
+                request.meta['page'] = page
+                request.meta['retry'] = retry
+                request.meta['uid'] = uid
+                yield request
+
                 return
+            else:
+                raise CloseSpider(resp['error'])
         else:
-            for status in resp["statuses"]:
-                try:
-                    weibo = WeiboItem()
-                    user = UserItem()
-
-                    weibo['created_at'] = status["created_at"]
-                    weibo['id'] = status['id']
-                    weibo['text'] = status['text']
-                    weibo['source'] = status['source']
-                    weibo['comments_count'] = status['comments_count']
-
-                    user['uid'] = status['user']['id']
-                    user['name'] = status['user']['name']
-                    user['gender'] = status['user']['gender']
-                    user['location'] = status['user']['location']
-                    user['verified'] = status['user']['verified']
-
-                    weibo['user'] = user
-
-                    if 'retweeted_status' in status:
-                        if 'deleted' in status['retweeted_status']:
-                            continue
-
-                        retweeted_user = UserItem()
-                        retweeted_user['name'] = status['retweeted_status']['user']['name']
-                        retweeted_user['uid'] = status['retweeted_status']['user']['id']
-                        retweeted_user['gender'] = status['retweeted_status']['user']['gender']
-                        retweeted_user['location'] = status['retweeted_status']['user']['location']
-                        retweeted_user['verified'] = status['retweeted_status']['user']['verified']
-
-                        weibo['retweeted_status'] = WeiboItem()
-                        weibo['retweeted_status']['user'] = retweeted_user
-
-                    yield weibo
-                except KeyError:
+            if resp.get('statuses') == []:
+                return
+            for status in resp['statuses']:
+                items = resp2item_v2(status)
+                if items == []:
                     continue
+                for item in items:
+                    yield item
 
             page += 1
             retry = 0
 
-        request = Request(BASE_URL.format(uid=uid, page=page,
-                          since_id=self.since_id, max_id=self.max_id),
-                          headers=None, callback=self.parse_next, dont_filter=True)
+            request = Request(BASE_URL.format(uid=uid, page=page,
+                              since_id=self.since_id, max_id=self.max_id), headers=None,
+                              callback=self.parse_next, dont_filter=True)
 
-        request.meta['page'] = page
-        request.meta['retry'] = retry
-        request.meta['uid'] = uid
-
-        yield request
+            request.meta['page'] = page
+            request.meta['retry'] = retry
+            request.meta['uid'] = uid
+            yield request
 
     def prepare(self):
         host = settings.get("REDIS_HOST", REDIS_HOST)
         port = settings.get("REDIS_PORT", REDIS_PORT)
+        api_key = settings.get("API_KEY", API_KEY)
+        self.r = _default_redis(host, port)
+        self.req_count = _default_req_count(r=self.r, api_key=api_key)
+        self.tk_alive = _default_tk_alive(r=self.r, api_key=api_key)
+
         uids_set = UIDS_SET.format(spider=self.name)
-
-        self.r = redis.Redis(host, port)
-
-        log.msg("load uids from {uids_set}".format(uids_set=uids_set), level=log.INFO)
+        log.msg('load uids from {uids_set}'.format(uids_set=uids_set), level=log.INFO)
         uids = self.r.smembers(uids_set)
-        if len(uids) == 0:
-            log.msg("{spider}: NO USER IDS TO LOAD".format(spider=self.name), level=log.WARNING)
+        if uids == []:
+            log.msg('{spider}: NO USER IDS TO LOAD'.format(spider=self.name), level=log.WARNING)
 
         return uids
